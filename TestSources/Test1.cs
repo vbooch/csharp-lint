2555f729bc5fe53b41a98b9051f9ee90792e16a2
bukin <vladimir.bukin@paragon-software.com>
2016-12-01 19:49:21 +0300

    Extended test source

--- TestSources/Test1.cs
+++ TestSources/Test1.cs
@@ -1,4 +1,20 @@
-public static class Test1
+using System.Diagnostics.CodeAnalysis;
+
+namespace MyNamespace
 {
-    public static string test
+    public static class Test1
+    {
+        public static string Test;
+    }
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
+    public static class Test2
+    {
+        public static string test;
+    }
+
+    public static class Test3
+    {
+        public static string Test;
+    }
 }
